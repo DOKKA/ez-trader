4a7629034e51c35ace0deb8e7df57c639684001f
Kevin Grieger <scuffmark@gmail.com>
2018-01-28 04:22:07 -0600

    initial commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,27 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  price: number;
+  amount: number;
+  limitPercentage: number;
 
+  baseData: Array<string>;
+  coinData: Array<string>;
+  base: string;
+  coin: string;
+  mode: boolean;
+
+  constructor(public navCtrl: NavController) {
+    this.baseData = ['BTC','ETH','USDT'];
+    this.coinData = ['STRAT','ETH','ICO','XRB','XLM'];
+    this.base = 'BTC';
+    this.coin = 'XLM';
+    this.price = .003;
+  }
+
+  getLimitPrice =() => {
+    let percentage = this.limitPercentage/100+1;
+    return parseFloat((percentage * this.price).toFixed(8));
   }
 
 }
